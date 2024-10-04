2b940b4720b5ac5dc21c6e501fa06f183d5bbc06
PranaviJadhav5774 <jadhavpranavi5774@gmail.com>
2024-10-04 09:05:27 +0530

    Add files via upload

---
+++ intrest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int radius, dimeter;
+    float area, perimeter;
+    printf()
+    scanf ("Enter radiud");
+    scanf("%d",&radius);
+    dimeter=2*radius;
+    perimeter=2*3/4*radius;
+    area=3/4*radius*radius;
+    printf("dimeter=d%n perimeter=%f/n area=%f" ,dimeter,perimeter,area);
+}
+
+
+
+
+
